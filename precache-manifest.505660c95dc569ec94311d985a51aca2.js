d850964692e0c50b1dd5bd24eafe0e6b90825920
Smita Narayan <snarayan@cimpress.com>
2020-06-07 04:16:02 -0400

    Updates

---
+++ precache-manifest.505660c95dc569ec94311d985a51aca2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5871daf50634c5ecb7ef0026ea4d81e8",
+    "url": "/tapeditor/index.html"
+  },
+  {
+    "revision": "ef270aca5b16f2a6729b",
+    "url": "/tapeditor/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e71af5cfe40eba97d3b0",
+    "url": "/tapeditor/static/js/2.bebf58ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tapeditor/static/js/2.bebf58ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef270aca5b16f2a6729b",
+    "url": "/tapeditor/static/js/main.719081fc.chunk.js"
+  },
+  {
+    "revision": "fe9696672f0c9983acab",
+    "url": "/tapeditor/static/js/runtime-main.362c23c9.js"
+  },
+  {
+    "revision": "e20e10a6ce641d93266ea09cbb867814",
+    "url": "/tapeditor/static/media/logo.e20e10a6.png"
+  }
+]);
