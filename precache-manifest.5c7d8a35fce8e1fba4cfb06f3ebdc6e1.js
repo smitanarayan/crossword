0f718ebd1e2dda4f11054f7cf804a9fdff0e7564
Smita Narayan <snarayan@cimpress.com>
2020-06-07 03:55:34 -0400

    Updates

---
+++ precache-manifest.5c7d8a35fce8e1fba4cfb06f3ebdc6e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69523a2f00f85c2cf7311a9e31ec5d90",
+    "url": "/smitanarayan/index.html"
+  },
+  {
+    "revision": "10171a8d8fd0edd31b76",
+    "url": "/smitanarayan/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d37e77ff4cffd5ea6e1e",
+    "url": "/smitanarayan/static/js/2.e5b8944c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/smitanarayan/static/js/2.e5b8944c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10171a8d8fd0edd31b76",
+    "url": "/smitanarayan/static/js/main.26efd868.chunk.js"
+  },
+  {
+    "revision": "aa98277c141f986c189c",
+    "url": "/smitanarayan/static/js/runtime-main.30e31875.js"
+  },
+  {
+    "revision": "e20e10a6ce641d93266ea09cbb867814",
+    "url": "/smitanarayan/static/media/logo.e20e10a6.png"
+  }
+]);
