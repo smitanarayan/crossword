982526cbaebddd70031c06bf637bcd065cdb1051
Smita Narayan <snarayan@cimpress.com>
2020-06-07 04:37:25 -0400

    Updates

---
+++ precache-manifest.9b28ebea38c645cd9a8e316eefcc0b9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0839e95c2c077120acca403f93fbd214",
+    "url": "/crossword/index.html"
+  },
+  {
+    "revision": "df81a70ca720b81acd86",
+    "url": "/crossword/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b088a15338864b443203",
+    "url": "/crossword/static/js/2.e367b8a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crossword/static/js/2.e367b8a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df81a70ca720b81acd86",
+    "url": "/crossword/static/js/main.7408a2ba.chunk.js"
+  },
+  {
+    "revision": "5eafbf393557ba1e36a9",
+    "url": "/crossword/static/js/runtime-main.fdd6e379.js"
+  },
+  {
+    "revision": "e20e10a6ce641d93266ea09cbb867814",
+    "url": "/crossword/static/media/logo.e20e10a6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tapeditor/precache-manifest.505660c95dc569ec94311d985a51aca2.js"
+  "/crossword/precache-manifest.9b28ebea38c645cd9a8e316eefcc0b9e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tapeditor/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crossword/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
